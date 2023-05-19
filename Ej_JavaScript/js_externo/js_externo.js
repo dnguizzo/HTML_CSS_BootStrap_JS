343c1fce42ac747a4555b77c9a31cdbdea98ad8f
113196_GUIZZO <113196@tecnicatura.frc.utn.edu.ar>
2023-05-18 22:35:31 -0300

    HTML-JS-JQ-Boot

---
+++ Ej_JavaScript/js_externo/js_externo.js
@@ -0,0 +1,13 @@
+/*       Establecemos una alerta */
+alert("Bienvenidos alumnos al curso");
+// Establecemos estilos
+document.getElementById("parrafo1").style.color = "red";
+document.getElementById("parrafo2").style.color = "blue";
+// Establecemos una funcion para una alerta
+function saludarme() {
+  alert("Bienvenido");
+}
+// Establecemos una funcion para cambiar estilo
+function cambiarEstilo() {
+  document.getElementById("parrafo3").style.color = "blue";
+}
